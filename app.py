d6e05b5a2a3b569a044431f9fe4317481af6f493
Roskud <babanealka@gmail.com>
2026-01-28 20:28:25 +0000

    Prepare app for Render deploy

--- app.py
+++ app.py
@@ -42,4 +42,6 @@ def index():
     )
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5002, debug=True)
+    import os
+port = int(os.environ.get("PORT", 5002))
+app.run(host="0.0.0.0", port=port)
